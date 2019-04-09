e81aacaf34ac819f1b4a51b01e7c86fe5e82d8ef
nicktorwald <nicktorwald@gmail.com>
2019-04-09 21:10:10 +0700

    Extract JDBC holdability check

---
+++ src/main/java/org/tarantool/util/JdbcConstants.java
@@ -0,0 +1,17 @@
+package org.tarantool.util;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLNonTransientException;
+import java.sql.Statement;
+
+public class JdbcConstants {
+
+    public static void checkHoldabilityConstant(int holdability) throws SQLException {
+        if (holdability != ResultSet.CLOSE_CURSORS_AT_COMMIT
+                && holdability != ResultSet.HOLD_CURSORS_OVER_COMMIT) {
+            throw new SQLNonTransientException("", SQLStates.INVALID_PARAMETER_VALUE.getSqlState());
+        }
+    }
+
+}
